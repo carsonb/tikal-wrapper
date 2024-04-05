7dfa7368754d0949cd0b550f0905b12c6e1b682a
Carson Brown <carson@carsonbrown.ca>
2024-04-05 15:08:55 -0400

    Adds an instructional step

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <ExtractFile />
+    <h1>Edit XLIFF</h1>
+    <p>Add translations into this file. <code>ES</code> only for the moment!</p>
     <MergeTranslation />
   </div>
 </template>
